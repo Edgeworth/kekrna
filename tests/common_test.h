367b11785d2012f6ff0f5219d6db4535317395b2
Eliot Courtney <eliot.daniel.courtney@gmail.com>
2016-08-07 12:48:19 +0800

    Move AU/GU penalty to globals.

---
+++ tests/common_test.h
@@ -0,0 +1,12 @@
+#ifndef KEKRNA_COMMON_TEST_H
+#define KEKRNA_COMMON_TEST_H
+
+namespace kekrna {
+namespace test {
+
+const uint32_t T04_HACK_MODEL_HASH = 0x3951E36A;
+
+}
+}
+
+#endif //KEKRNA_COMMON_TEST_H
