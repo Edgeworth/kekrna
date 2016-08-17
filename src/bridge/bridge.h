62230b3c5bdd8f6ff82c797be167621eecd77a79
Eliot Courtney <eliot.daniel.courtney@gmail.com>
2016-08-17 19:23:28 +0900

    Rename some variables.

--- src/bridge/bridge.h
+++ src/bridge/bridge.h
@@ -13,17 +13,22 @@ class RnaPackage {
 public:
 
   virtual ~RnaPackage() = default;
+
   virtual energy_t Efn(const folded_rna_t& frna, std::string* desc = nullptr) const = 0;
+
   virtual folded_rna_t Fold(const rna_t& rna) const = 0;
 };
 
 class Rnark : public RnaPackage {
 public:
   Rnark(const std::string& data_path);
+
   Rnark(const Rnark&) = delete;
+
   Rnark& operator=(const Rnark&) = delete;
 
   virtual energy_t Efn(const folded_rna_t& frna, std::string* desc = nullptr) const;
+
   virtual folded_rna_t Fold(const rna_t& rna) const;
 
 private:
@@ -33,11 +38,15 @@ private:
 class Rnastructure : public RnaPackage {
 public:
   Rnastructure(const std::string& data_path, bool _use_lyngso);
+
   Rnastructure(const Rnastructure&) = delete;
+
   Rnastructure& operator=(Rnastructure&) = delete;
 
   virtual energy_t Efn(const folded_rna_t& frna, std::string* desc = nullptr) const;
+
   virtual folded_rna_t Fold(const rna_t& rna) const;
+
 private:
   std::unique_ptr<datatable> data;
   bool use_lyngso;
@@ -46,10 +55,13 @@ private:
 class Kekrna : public RnaPackage {
 public:
   Kekrna(const std::string& data_path);
+
   Kekrna(const Kekrna&) = delete;
+
   Kekrna& operator=(const Kekrna&) = delete;
 
   virtual energy_t Efn(const folded_rna_t& frna, std::string* desc = nullptr) const;
+
   virtual folded_rna_t Fold(const rna_t& rna) const;
 };
 
